c598819730abc88ce4d67bb25d13cf461d1ee592
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-11-18 23:24:48 -0500

    added permission constants that will be used for permissions seeding and management;

---
+++ app/Constants/PermissionConstants.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Constants;
+
+final class PermissionConstants
+{
+    // User
+    const CREATE_USER = 'create-user';
+    const GET_USER = 'get-user';
+    const FIND_USER = 'find-user';
+    const UPDATE_USER = 'update-user';
+    const DELETE_USER = 'delete-user';
+
+    // Role
+    const CREATE_ROLE = 'create-role';
+    const GET_ROLE = 'get-role';
+    const FIND_ROLE = 'find-role';
+    const UPDATE_ROLE = 'update-role';
+    const DELETE_ROLE = 'delete-role';
+
+    // Permission
+    const GET_PERMISSION = 'get-PERMISSION';
+    const FIND_PERMISSION = 'find-PERMISSION';
+    const UPDATE_PERMISSION = 'update-PERMISSION';
+}
