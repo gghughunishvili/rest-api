4bc41446912f061bbab7d1ef5112d15fc6c04316
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-16 23:45:24 -0500

    removed default users endpoint; added issue token endpoint and accessTokenController; added payload to issue token test; #1

--- tests/Feature/Auth/IssueTokenTest.php
+++ tests/Feature/Auth/IssueTokenTest.php
@@ -14,7 +14,15 @@ class IssueTokenTEst extends TestCase
      */
     public function unauthorized_user_should_not_get_a_token()
     {
-        $response = $this->json($this->method, $this->endpoint);
+        $payload = [
+            'client_id' => $this->faker->randomNumber,
+            'client_secret' => $this->faker->word,
+            'grant_type' => 'password',
+            'username' => $this->faker->userName,
+            'password' => $this->faker->password
+
+        ];
+        $response = $this->json($this->method, $this->endpoint, $payload);
         $response->assertStatus(401);
     }
 }
