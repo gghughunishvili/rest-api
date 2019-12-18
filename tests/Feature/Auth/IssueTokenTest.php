27d984de80be281d63b0013354ee093973c207c1
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2019-12-18 00:49:27 -0500

    improved tests; added checking that password grants work with correct credentials; #1

--- tests/Feature/Auth/IssueTokenTest.php
+++ tests/Feature/Auth/IssueTokenTest.php
@@ -3,21 +3,26 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\OauthClient;
+use App\Models\User;
 use Tests\TestCase;
 
 class IssueTokenTEst extends TestCase
 {
     protected $endpoint = 'api/v1/oauth/token';
     protected $method = 'POST';
-    protected $jsonStrucutre = [
+    protected $clientCredentialsJsonStructure = [
         'access_token',
         'token_type',
         'expires_in',
     ];
+    protected $passwordJsonStructure = [
+        'access_token',
+        'token_type',
+        'expires_in',
+        'refresh_token',
+    ];
 
-    /**
-     * @test
-     */
+    /** @test */
     public function it_should_not_return_token_with_wrong_credentials()
     {
         $payload = [
@@ -32,15 +37,10 @@ class IssueTokenTEst extends TestCase
         $response->assertStatus(401);
     }
 
-    /**
-     * @test
-     */
+    /** @test */
     public function it_should_generate_client_credentials_token_with_correct_credentials()
     {
-        $oauthClient = factory(OauthClient::class)->make([
-            'personal_access_client' => true,
-        ]);
-        $oauthClient->save();
+        $oauthClient = factory(OauthClient::class)->state('client_credentials')->create();
         $payload = [
             'client_id' => $oauthClient->id,
             'client_secret' => $oauthClient->secret,
@@ -48,6 +48,27 @@ class IssueTokenTEst extends TestCase
         ];
         $response = $this->json($this->method, $this->endpoint, $payload);
         $response->assertStatus(200);
-        $response->assertJsonStructure($this->jsonStrucutre);
+        $response->assertJsonStructure($this->clientCredentialsJsonStructure);
+    }
+
+    /** @test */
+    public function it_should_generate_password_token_with_correct_credentials()
+    {
+        $password = 'test';
+        $user = factory(User::class)->make([
+            'password' => bcrypt($password)
+        ]);
+        $user->save();
+        $oauthClient = factory(OauthClient::class)->state('password')->create();
+        $payload = [
+            'client_id' => $oauthClient->id,
+            'client_secret' => $oauthClient->secret,
+            'grant_type' => 'password',
+            'username' => $user->email,
+            'password' => $password,
+        ];
+        $response = $this->json($this->method, $this->endpoint, $payload);
+        $response->assertStatus(200);
+        $response->assertJsonStructure($this->passwordJsonStructure);
     }
 }
